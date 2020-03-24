d417ab5f930f750b72c1d41f26ecea6543354694
Alex Novacovschi <alex.novacovschi@endava.com>
2020-03-24 02:03:34 +0200

    changed kava page

--- assets/js/kava_calc.js
+++ assets/js/kava_calc.js
@@ -1,13 +1,15 @@
-$('#quantity').click(function () {
-    $('#sum').text($(this).val());
-});
+function multiplyBy()
+{
+        num1 = document.getElementById("kava_amount_calc").value;
+        num2 = document.getElementById("kava_comission_calc").value;
+        num3 = 0.075;
+        num4 = 100;
+        num5 = 365;
+        num6 = 12;
+        daily = num1 * num3 / num5 - num2 / num4 * num1 * num3 / num5;
+        daily_rewards = daily;
 
-$('#y').click(function () {
-    var y = document.querySelector('#y').value;
-    $('#sum').text($(this).val()).y;
-}); 
-
-$('#x').click(function () {
-    var x = document.querySelector('#x').value;
-    $('#sum').text($(this).val()).x;
-}); 
+        document.getElementById("kava_daily").innerHTML = daily_rewards;
+        document.getElementById("kava_monthly").innerHTML = num1 * num3 / num6 - num2 / num4 * num1 * num3 / num6;
+        document.getElementById("kava_yearly").innerHTML = num1 * num3 - num2 / num4 * num1 * num3;
+}
